4921954c970dbaef9c6098ed543151f2f918fd2b
Maria Rosaria <mariarstefanizzi@gmail.com>
2026-03-03 15:48:46 +0100

    prima stesura di dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,18 @@
+
 class Dictionary:
     def __init__(self):
-        pass
+        self.dict = {}
 
-    def addWord(self):
-        pass
+    def addWord(self, alien_word, translations):
+        if alien_word in self.dict:
+            for t in translations:
+                if t not in self.dict[alien_word]:
+                    self.dict[alien_word].append(t)
+        else:
+            self.dict[alien_word] = list(translations)
 
-    def translate(self):
-        pass
+    def translate(self, alien_word):
+        return self.dict.get(alien_word, None)
 
-    def translateWordWildCard(self):
+    def translateWordWildCard(self, query):
         pass
